f5c1a5dc2261ea5271743a4e55cb3b3b0dca4ca8
612528619 <mudireddyramanjaneyulu.reddy@bt.com>
2020-07-17 14:20:05 +0530

    Commit test

---
+++ src/main/java/com/example/people10/People10Application.java
@@ -0,0 +1,13 @@
+package com.example.people10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class People10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(People10Application.class, args);
+	}
+
+}
